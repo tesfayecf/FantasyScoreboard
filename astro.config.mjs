92c7607dd4e49d51105d8b6fcb8190ec8c205730
tesfayecf <90719152+tesfayecf@users.noreply.github.com>
2024-08-31 10:46:16 +0200

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
 import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind()],
+  integrations: [icon(), tailwind(), preact()],
   output: "hybrid",
   adapter: vercel(),
-  outDir: "./dist",
 });
